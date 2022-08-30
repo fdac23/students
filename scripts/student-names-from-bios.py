3e21152368bbd7ee84a6cfc5eb4a0d9f7c9524ba
Ben Klein <robobenklein@gmail.com>
2022-08-30 15:49:51 -0400

    autorun scripts

--- scripts/student-names-from-bios.py
+++ scripts/student-names-from-bios.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import roboyml
 from settings import *
 
-find_name_re = r"^((Hi|Hey)!?,? |Hello[\., ]?(there)?,? ?)?(I'm |I am |[Mm]y name is )(?P<firstname>(([A-Z]\w+)[ ]?)+?)\W(?P<lastname>[A-Z]\w+)"
+find_name_re = r"^ *((Hi|Hey)!?,? |Hello[\., ]?(there)?,? ?)?(I'm |I am |[Mm]y name is )(?P<firstname>(([A-Z]\w+)[ ]?)+?)\W(?P<lastname>[A-Z]\w+)"
 
 with roboyml.open(studentfile) as students:
   for netid, student in students.items():

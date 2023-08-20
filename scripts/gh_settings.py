21010e9932cca0a93eb5bcb39ebc328de6e8f8cb
Ben Klein <robobenklein@gmail.com>
2023-08-20 18:43:00 -0400

    update gh_settings

--- scripts/gh_settings.py
+++ scripts/gh_settings.py
@@ -2,12 +2,12 @@
 from github import Github
 from dotenv import load_dotenv
 
-from settings import *
+from .settings import *
 
 load_dotenv()
 
 gh = Github(os.environ.get("GITHUB_TOKEN"))
 
-class_org = gh.get_organization("fdac22")
+class_org = gh.get_organization("fdac23")
 class_repo = class_org.get_repo("students")
 

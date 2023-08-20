0478b22fe328ed5068eb089a75c5daff38edb930
Ben Klein <robobenklein@gmail.com>
2023-08-20 19:14:17 -0400

    use GITHUB_REPOSITORY

--- scripts/actions_handler.py
+++ scripts/actions_handler.py
@@ -13,10 +13,14 @@ parser.add_argument("github_login")
 parser.add_argument("file_changed")
 args = parser.parse_args()
 
+gh_org_name, gh_repo_name = os.environ.get("GITHUB_REPOSITORY").split("/")
+
+
 with roboyml.open(studentfile) as students:
     netid = Path(args.file_changed).stem.lower()
     if netid not in students:
         students[netid] = {}
     if "github" not in students[netid]:
         students[netid]["github"] = args.github_login
+
 
--- scripts/gh_settings.py
+++ scripts/gh_settings.py
@@ -8,6 +8,9 @@ load_dotenv()
 
 gh = Github(os.environ.get("GITHUB_TOKEN"))
 
-class_org = gh.get_organization("fdac23")
+class_org = gh.get_organization(os.environ.get("GITHUB_REPOSITORY_OWNER", "fdac23"))
 class_repo = class_org.get_repo("students")
 
+if __name__ == "__main__":
+    print(f"org: {class_org}")
+    print(f"repo: {class_repo}")
